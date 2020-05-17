f14c5248075b694d675cc312efa67cbb924b6533
Deng An Jia <denganjia111@gmail.com>
2020-05-17 16:33:50 +0800

    修改了登录页面的外观

---
+++ static/js/login.js
@@ -0,0 +1,31 @@
+function f() {
+    const input_user = document.getElementById("account");
+    input_user.setAttribute("value", "username");
+
+    input_user.onselect = function () {
+        input_user.setAttribute("value", "");
+    }
+    input_user.onfocus = function () {
+        input_user.setAttribute("value", "");
+    }
+    input_user.onblur = function () {
+        input_user.setAttribute("value", "username");
+    }
+
+    const input_password = document.getElementById("password");
+    input_password.setAttribute("type","text");
+    input_password.setAttribute("value","password");
+    input_password.onfocus = function () {
+        input_password.setAttribute("type","password");
+        input_password.setAttribute("value","");
+    }
+    input_password.onselect = function () {
+        input_password.onfocus(undefined);
+    }
+    input_password.onblur = function () {
+        input_password.setAttribute("type","text");
+        input_password.setAttribute("value","password");
+    }
+}
+
+
